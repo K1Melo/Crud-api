c3497695e562d957a6ccbdedc9bad0d7f389c86c
Kaua <k1fernandesyt@gmail.com>
2023-08-13 10:42:24 -0300

    Connection with front

--- src/main/java/com/projectcrud/crud/controller/ProductsController.java
+++ src/main/java/com/projectcrud/crud/controller/ProductsController.java
@@ -1,6 +1,7 @@
 package com.projectcrud.crud.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,22 +22,26 @@ public class ProductsController {
 	@Autowired
 	private ProductRepository productRepository;
 	
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@GetMapping
 	public Iterable<Product> getAllProducts() {
 		return productRepository.findAll();
 	}
 	
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@GetMapping(path = "/{id}")
 	public void getElementById(@PathVariable int id) {
 		productRepository.findById(id);
 	}
 	
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@RequestMapping(method = {RequestMethod.POST, RequestMethod.PUT})
 	public Product saveProduct(@Valid Product product) {
 		productRepository.save(product);
 		return product;
 	}
 	
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@DeleteMapping
 	public void deleteProduct(@RequestParam int id) {
 		productRepository.deleteById(id);
